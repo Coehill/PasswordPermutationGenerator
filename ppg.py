08985f5f192c35b919bc30e6fe1e1e352bac9e2f
DistantParadox <craigtski47@gmail.com>
2016-11-07 10:42:27 -0500

    Added min/max functionality

--- ppg.py
+++ ppg.py
@@ -11,6 +11,8 @@ inputName = "intput.lst"
 outputName = "output.lst"
 maxSize = -1
 permutations = []
+minl = -1
+maxl = -1
 
 
 def usage():
@@ -31,20 +33,22 @@ def main():
     global outputName
     global maxSize
     global permutations
+    global minl
+    global maxl
     
     if not len(sys.argv[1:]):
         usage()
         
     # read the command line options
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"h:i:o:b",
-                                   ["help","input-name", "output-name", "max-size"])
+        # if one of the options requires an arg, then it's followed by a colon
+        opts, args = getopt.getopt(sys.argv[1:],"hi:o:b:m:x:",
+                                   ["help","input-name=", "output-name=", "max-size=", "min=", "max="])
     except getopt.GetoptError as err:
         print(str(err))
         usage()
         
     for o,a in opts:
-        print(a)
         if o in ("-h", "--help"):
             usage()
         elif o in ("-i", "--input-name"):
@@ -53,6 +57,10 @@ def main():
             outputName = a
         elif o in ("-b", "--max-size"):
             maxSize = a
+        elif o in ("-m", "--min"):
+            minl = int(a)
+        elif o in ("-x", "--max"):
+            maxl = int(a)
         else:
             print("assert false")
             assert False,"Unhandled Option"
@@ -64,7 +72,18 @@ def main():
         
     generate(lines)
     
+    formatSize()
+    
+    print(permutations)
+    
         
+'''
+Given a list of strings, generate permutations of those strings and store
+in the global list Permutations.  This method has the permutations residing
+in memory, so the size of the wordlist will depend on your hardware.  In
+the future, this should be optimized to write directly to an external file
+to remove the dependance on physical memory.
+'''
 def generate(lines):
     global inputName
     global outputName
@@ -75,9 +94,40 @@ def generate(lines):
         # work in progress
         print(list(itertools.permutations(lines, maxSize)))
     else:
-        permutations = list(itertools.permutations(lines))
-        print(permutations)
+        # create permutations
+        tempList = list(itertools.permutations(lines))
+        
+        # convert permutation tuples into strings
+        for tuple in tempList:
+            a = ""
+            for tupleItem in tuple:
+                a = a + tupleItem
+            permutations.append(a)
+            
+        # print(permutations)
+        
+
+'''
+Using the global permutations list, remove necessary items to conform to the
+size restrictions in min and max
+'''
+def formatSize():
+    global permutations
+    global minl
+    global maxl
     
+    tempList = []
+    if minl > 0:
+        for item in permutations:
+            if len(item) >= minl:
+                tempList.append(item)
+    permutations = tempList
+    tempList = []
+    if maxl > 0:
+        for item in permutations:
+            if len(item) <= maxl:
+                tempList.append(item)
+    permutations = tempList
     
     
         
